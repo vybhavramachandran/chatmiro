161db4ccc1b36fbbe6094d4e673ffdb586204962
Vybhav Ramachandran <vybhav@Vybhavs-Mac-mini.local>
2023-03-17 08:43:53 +0530

    first commit

---
+++ src/api.jsx
@@ -0,0 +1,87 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://api.openai.com/v1/';
+const API_KEY = localStorage.getItem('chatMiroAPIKey');
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${API_KEY}`
+  };
+  
+
+
+
+  const retreiveMindMapFromOpenAI = async (prompt) => {
+
+    var data = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: `
+      You are an expert in everything and your goal is to help me visually learn a topic. Be as descriptive as possible. Go as broad or deep as needed so that I fully learn a concept.
+  
+      When I provide a prompt, return a response in JSON format that generates a mindmap of topics inside it. 
+      
+      I don't want you to return a JSON, but the response should be structured like a JSON. 
+      
+      Note:
+      1. No other data, except the JSON structured data should be present in the response. All returned response must be inside the JSON.
+      2. Don't include any quotes inside the text content
+      
+      The PROMPT is "${prompt}". Go atleast 4 levels deep. Try to explain as much as possible.
+      
+      Here's a sample structure.
+      {
+      "root": {
+      "text": "Root Node",
+      "children": [
+      {
+      "text": "Child Node",
+      "children": []
+      },
+      {
+      "text": "Child Node",
+      "children": [
+      {
+      "text": "Child Node",
+      "children": [
+        {
+          "text": "Child Node",
+          "children": []
+          },
+          {
+            "text": "Child Node",
+            "children": [
+              {
+                "text": "Child Node",
+                "children": []
+                },
+                {
+                  "text": "Child Node",
+                  "children": []
+                  },
+            ]
+            },
+      ]
+      },
+      {
+      "text": "Child Node",
+      "children": []
+      }
+      ]
+      }
+      ]
+      }
+      }` }],
+      temperature: 0.7
+    };
+  
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', data, { headers });
+      console.log(response.data);
+      return response.data.choices[0].message.content.trim();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+export { retreiveMindMapFromOpenAI };
--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,319 @@
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
+import { useState,useEffect } from 'react';
+import { retreiveMindMapFromOpenAI } from './api.jsx';
+import { FaSpinner } from 'react-icons/fa';
+import tinycolor from 'tinycolor2';
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
 
-  await miro.board.viewport.zoomTo(stickyNote);
-}
+
+
 
 const App = () => {
+
+  const [apiKey, setApiKey] = useState('');
+  const [isValidApiKey, setIsValidApiKey] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [isEditing, setIsEditing] = useState(false)
+  var [lastYPos, setLastYPos] = useState(0)
+  var [userPrompt,setUserPrompt]=useState("")
+  var [buttonText,setButtonText]=useState("Generate Mindmap ⚡️")
+  var [isDrawingMindMap,setIsDrawingMindMap]=useState(false)
+
+  useEffect(() => {
+    console.log(`lastYPos: ${lastYPos}`);
+  }, [lastYPos]);
+
+  function validateApiKey(apiKey) {
+    const apiKeyRegex = /^sk-[a-zA-Z0-9]{48}$/;
+    const isValid = apiKeyRegex.test(apiKey.trim());
+    setIsValidApiKey(isValid);
+  }
+
+  const handleSubmit = () => {
+    if (apiKey.trim() === '') {
+      setError(true);
+    } else if (!isValidApiKey) {
+      setError(true);
+      setApiKey('');
+    } else {
+      try {
+        localStorage.setItem('chatMiroAPIKey', apiKey.trim());
+        setSuccess(true);
+        setEditable(false);
+        setError(false);
+      } catch (e) {
+        console.error('Error saving API key to local storage:', e);
+        setError(true);
+        setApiKey('');
+      }
+    }
+  };
+
+const generateRandomColor = (parentColor) => {
+  if (!parentColor) {
+    // If parentColor is empty, generate a random color
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 20) + 80;
+    const brightness = Math.floor(Math.random() * 20) + 80;
+    return tinycolor({h: hue, s: saturation, l: brightness}).toHexString();
+  }
+
+  // If parentColor is not empty, use it to generate a new color
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * 20) + 80;
+  const brightness = Math.floor(Math.random() * 20) + 80;
+  const parentColorObj = tinycolor(parentColor);
+  const newColorObj = parentColorObj.spin(hue).saturate(saturation).lighten(brightness);
+  return newColorObj.toHexString();
+};
+
+
+  async function createMindMapFromResponse(response) {
+    setLastYPos(0);
+    console.log('response', response.root)
+    if (response && Object.keys(response).length > 0) {
+      const rootNodeName = response.root.text;
+      const rootNode = await miro.board.createShape({
+        shape: 'round_rectangle',
+        content: rootNodeName,
+        x: 0,
+        y: 0,
+        style: {
+          color:'#ffffff',
+          fillColor:'#36A8EF',
+          borderWidth:2,
+          borderColor:'#0056FF'
+        }
+      });
+      await miro.board.viewport.zoomTo(rootNode);
+      await miro.board.viewport.setZoom(1);
+      const childNodes = response.root.children;
+      if (childNodes) {
+        const createChildNodes = async (parentNode, childNodes, isLeft, currentYPos, parentColor,isRoot) => {
+          console.log('currentYPos:', currentYPos);
+
+          if (!childNodes) {
+            return currentYPos;
+          }
+          const childNodeNames = Object.keys(childNodes);
+          const childCount = childNodeNames.length;
+          const totalChildHeight = childCount * 100; // adjust this value as needed
+          const spacing = totalChildHeight / childCount;
+          const sideMultiplier = isLeft ? -1 : 1;
+          const sideOffset = isLeft ? -150 : 150;
+          
+          for (let i = 0; i < childNodeNames.length; i++) {
+            const childNodeName = childNodeNames[i];
+            const childNodeColor = isRoot ? generateRandomColor() : generateRandomColor(parentColor); // generate random color based on parent color
+            console.log("childNodeName ",childNodes[childNodeName].text);
+            const childNode = await miro.board.createShape({
+              shape: 'round_rectangle',
+              content: childNodes[childNodeName].text || '',
+              x: parentNode.x + sideOffset,
+              y: currentYPos + spacing * i,
+              style: {
+                fillColor: childNodeColor // set the fill color of the child node
+              }
+            });
+            await miro.board.createConnector({
+              shape: 'elbowed',
+              style: {
+                endStrokeCap: 'stealth',
+                strokeStyle: 'dotted',
+                strokeColor: '#ff00ff',
+                strokeWidth: 2,
+              },
+              start: {
+                item: parentNode.id,
+                position: {
+                  x: isLeft ? 0.0 : 1.0,
+                  y: 0.5,
+                },
+              },
+              end: {
+                item: childNode.id,
+                snapTo: isLeft ? 'right' : 'left',
+              }
+            });
+            const nextYPos = await createChildNodes(childNode, childNodes[childNodeName].children, isLeft, currentYPos + spacing * i, childNodeColor,false); // pass the child color to the next level of descendants
+            currentYPos = Math.max(currentYPos, nextYPos);
+          }
+          return currentYPos;
+        };
+        const leftChildren = {};
+        const rightChildren = {};
+        Object.keys(childNodes).forEach((childNodeName, index) => {
+          const childNode = childNodes[childNodeName];
+          if (index % 2 == 0) {
+            leftChildren[childNodeName] = childNode;
+          } else {
+            rightChildren[childNodeName] = childNode;
+          }
+        });
+        const maxLeftYPos = await createChildNodes(rootNode, leftChildren, true, 0,"#36A8EF",true);
+        const maxRightYPos = await createChildNodes(rootNode, rightChildren, false,0,"#36A8EF",true);
+        setLastYPos(Math.max(maxLeftYPos, maxRightYPos));
+        setIsDrawingMindMap(false);
+      } else {
+        console.error('Invalid response from API');
+      }
+    }
+  }
+  
+  
+
+  
   React.useEffect(() => {
-    addSticky();
+    const storedApiKey = localStorage.getItem('chatMiroAPIKey');
+    console.log("Retreived key ",storedApiKey);
+    if (storedApiKey) {
+      setApiKey(storedApiKey);
+      console.log("Key set ",apiKey);
+      setSuccess(true);
+    }
   }, []);
 
   return (
     <div className="grid wrapper">
       <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
+        <img src="/src/assets/chatmiro.png" alt="" />
       </div>
       <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
+      <p class="p-large">
+            With ChatMiro, you can create mindmaps using the power of OpenAI's ChatGPT! 🤙
+          </p>
       </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
+      {success && !isEditing ? (
+        <div class="cs1 ce12 grid">
+          <div class="cs1 ce12">
+        
+            </div>
+          <div class="cs1 ce12">
+            <h3 class="h3" id="headings"> Step 1️⃣ : OpenAI API Key ✅ </h3>
+          </div>
+          <span className="cs1 ce10">
+            <p className="p-small" id="keyfield">
+            sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            </p>
+          </span>
+          <span className="cs11 ce12">
+            <button
+              class="button button-secondary button-small"
+              type="button"
+              onClick={() => setIsEditing(true)}
+            >
+              ✏️
+            </button>
+          </span>
+          {isValidApiKey && <div className="status-text">✅ Valid API Key</div>}
+          <div class="cs1 ce12">
+          <h3 class="h3" id="headings">Step 2️⃣: Generate Mindmap</h3>
+          {/* <p class="p-large">
+            With ChatMiro, you can create mindmaps using the power of OpenAI's ChatGPT.
+          </p> */}
+          <div class="form-group">
+	<label for="example-1">What would you like to know about?</label>
+	<input class="input" 
+   value={userPrompt}
+   onChange={(event) => {
+     setUserPrompt(event.target.value);
+   }}
+  type="text" placeholder="What would you like to know about?" id="example-1"/>
+</div>
+<div class="cs1 ce12" id="buttonclass">
+          <button
+          type="button"
+          disabled = {!userPrompt || isDrawingMindMap}
+          className="cs1 ce12 button button-secondary"
+          onClick={async () => {
+            setIsDrawingMindMap(true)
+            const mindmapText = await retreiveMindMapFromOpenAI(userPrompt);
+            await createMindMapFromResponse(JSON.parse(mindmapText));
+
+          }}
         >
-          Read the documentation
-        </a>
-      </div>
+          		{!isDrawingMindMap?"Generate Mindmap ⚡️":<FaSpinner className="loadingIcon"/>}
+
+        </button>
+        </div>
+          </div>
+        </div>
+      ) : (
+        <form className="cs1 ce12 form-example--main-content">
+          <div className="form-group">
+          <h3 class="h3" id="headings"> {isEditing? "Step 1️⃣ : OpenAI API Key ✏️" :"1️⃣ : OpenAI API Key "} </h3>
+
+            <p className="p-small">
+              <a href="https://platform.openai.com/account/api-keys" target="_blank">
+                Get your API key from OpenAI dashboard
+              </a>
+            </p>
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter your API key"
+              value={apiKey}
+              onChange={(event) => {
+                setApiKey(event.target.value);
+                validateApiKey(event.target.value);
+              }}
+              onBlur={() => validateApiKey(apiKey)}
+              disabled={!isEditing}
+            />
+  
+            {isValidApiKey && <div className="status-text">✅ Valid API Key</div>}
+          </div>
+          
+      
+        </form>
+        
+      )}
+      {/* {!success ? (
+        <button
+          type="button"
+          className="cs1 ce12 button button-primary"
+          onClick={handleSubmit}
+          disabled={!isValidApiKey}
+        >
+          Save API Key
+        </button>
+      ) : (
+        <h2 className="h2">OpenAI Key Saved!</h2>
+      )} */}
+      {isEditing && (
+        <span class="cs1 ce12">
+        <button
+          type="button"
+          className="cs1 ce12 button button-secondary"
+          onClick={() => {
+            setIsEditing(false);
+            setIsValidApiKey(false);
+            setSuccess(true);
+          }}
+        >
+          		<span class="icon-close"></span>
+
+        </button>
+        <button
+          type="button"
+          className="cs1 ce12 button button-secondary"
+          disabled={!isValidApiKey}
+          onClick={() => {
+            setIsEditing(false);
+            setIsValidApiKey(false);
+            localStorage.setItem("chatMiroAPIKey", apiKey.trim());
+            setSuccess(true);
+          }}
+        >
+          💾 Save
+        </button>
+        </span>
+      )}
     </div>
   );
+  
 };
 
 const container = document.getElementById('root');
